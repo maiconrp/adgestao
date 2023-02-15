7a98154e99f5ad2f7246ea6804c4dfb60777846a
Maicon Robert <maiconlk2321@gmail.com>
2023-02-15 15:50:39 -0300

    :wrench: chore: Add sede e multiselect fields in installed

--- code/ADGESTAO/settings.py
+++ code/ADGESTAO/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
     # Meus apps
     "igreja",
     "usuario",
+    "sede",
+    
+    # utils
     "multiselectfield",
 ]
 
