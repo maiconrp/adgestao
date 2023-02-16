3942bed908480a4fda1b6b2e6e97fd4aa40f60ed
Maicon Robert <maiconlk2321@gmail.com>
2023-02-16 00:20:04 -0300

    :wrench: build: Config urls para views

--- code/ADGESTAO/urls.py
+++ code/ADGESTAO/urls.py
@@ -34,6 +34,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('usuario.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     
 ]
--- code/usuario/urls.py
+++ code/usuario/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from . import views
 from django.contrib.auth.views import PasswordChangeView
- 
+
+from .views import *
+
 urlpatterns = [
-    path('', views.renderLP, name="renderLP"),
-    path('registrar_tesoureiro_sede/', views.criarTesoureiroSede, name="criarTesoureiroSede"),
-    path('registrar_pastor/', views.criarPastor, name="addTeacher"),
-    path('registrar_tesoureiro/', views.criarTesoureiro, name="criarTesoureiro"),
-    path('mudar_senha/', PasswordChangeView.as_view(template_name='usuario/mudarSenha.html')),
-    path('mudar_senha/done/', PasswordChangeView.as_view(template_name='home.html')),
- ]
+    path('',                                    renderLP, name="renderLP"),
+    path('mudar_senha/',                        PasswordChangeView.as_view(template_name='usuario/mudarSenha.html')),
+    path('solicitacoes/',                       listar_solicitacoes,    name='listar_solicitacoes'),
+    path('solicitacao/autorizar/<str:cpf>/',    autorizar_solicitacao,  name='autorizar_solicitacao'),
+    path('solicitacao/negar/<str:cpf>/',        negar_solicitacao,      name='negar_solicitacao'),
+    path('cadastro/solicitar/',                 solicitar_cadastro,     name='solicitar_cadastro'),
+    path('solicitacao/acompanhar/',             acompanhar_cadastro,    name='acompanhar_cadastro'),
+]
