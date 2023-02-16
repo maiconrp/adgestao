3290b198ae7f6613fce12e1f1a064777162d53d6
Maicon Robert <maiconlk2321@gmail.com>
2023-02-16 18:52:47 -0300

    :wrench: build: Add models in admin
    Co-authored-by: Victor Fonteles victorwilva@gmail.com

--- code/usuario/admin.py
+++ code/usuario/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(TesoureiroSede)
+admin.site.register(Tesoureiro)
+admin.site.register(Pastor)
+admin.site.register(SolicitacaoCadastro)
