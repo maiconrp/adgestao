cbbc35cc04515e1bbf56aa54ae78f80dfe9a165c
Maicon Robert <maiconlk2321@gmail.com>
2023-02-17 17:23:06 -0300

    :wrench: chore: Add path para modulo apps.<app>

--- code/adgestao/settings.py
+++ code/adgestao/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os, sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,9 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'apps.accounts',
-    'apps.igreja'
-    'apps.financas',
+    
+    # Meus apps
+    'accounts',
+    'igreja',
+    'financas',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +127,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, '../apps'))
