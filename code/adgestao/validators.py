b8c7e406d752e625001a51a92b3d4b7bd69b3322
Maicon Robert <maiconlk2321@gmail.com>
2023-02-17 22:54:59 -0300

    :tada: progress: Create validators

---
+++ code/adgestao/validators.py
@@ -0,0 +1,31 @@
+from django.core.exceptions import ValidationError
+import phonenumbers
+
+
+def validate_cpf(value):
+    # Lógica para validação do CPF
+    if not value.isdigit():
+        raise ValidationError('O CPF deve conter apenas números.')
+    if len(value) != 11:
+        raise ValidationError('O CPF deve ter 11 dígitos.')
+    
+    # Se o CPF é inválido
+    if invalid:
+        raise ValidationError('CPF inválido.')
+
+def validate_data(value):
+    if value is not None:
+        if not isinstance(value, str):
+            value = value.strftime('%Y-%m-%d')
+        try:
+            datetime.datetime.strptime(value, '%Y-%m-%d')
+        except ValueError:
+            raise ValidationError('Data de nascimento inválida')
+
+def validate_telefone(value):
+    try:
+        parsed_number = phonenumbers.parse(value, "BR")
+        if not phonenumbers.is_valid_number(parsed_number):
+            raise ValidationError("Número de telefone inválido.")
+    except phonenumbers.NumberParseException:
+        raise ValidationError("Número de telefone inválido.")
