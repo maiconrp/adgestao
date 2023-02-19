ae22e162d8f5b5ec876de0c8ac7f52d380745b67
Maicon Robert <maiconlk2321@gmail.com>
2023-02-18 22:11:04 -0300

    :white_check_mark: test: Teste de permissões comentado

--- code/adgestao/tests/permissions.py
+++ code/adgestao/tests/permissions.py
@@ -5,13 +5,24 @@ from accounts.models import Usuario
 from igreja.models import Igreja
 from accounts.permissions import set_permission
 
+
 class SetPermissionTestCase(TestCase):
+    """
+    Teste para a função de permissões `set_permission`.
+    """
+
     def setUp(self):
+        """
+        Configura os dados iniciais do teste.
+        """
+        # Cria uma Igreja para os usuários
         self.igreja, self._ = Igreja.objects.get_or_create(
             nome="Igreja Teste",
             localizacao="Local de teste",
             saldo=0
-            )
+        )
+        
+        # Cria um usuário Tesoureiro Sede
         self.usuario_ts, self._ = Usuario.objects.get_or_create(
             nome='Tesoureiro Sede',
             cpf='12345678901',
@@ -19,6 +30,8 @@ class SetPermissionTestCase(TestCase):
             funcao='TS',
             igreja=self.igreja,
         )
+        
+        # Cria um usuário Tesoureiro
         self.usuario_t, self._  = Usuario.objects.get_or_create(
             nome='Tesoureiro',
             cpf='23456789012',
@@ -26,6 +39,8 @@ class SetPermissionTestCase(TestCase):
             funcao='T',
             igreja=self.igreja,
         )
+        
+        # Cria um usuário Pastor
         self.usuario_p, self._  = Usuario.objects.get_or_create(
             nome='Pastor',
             cpf='34567890123',
@@ -35,16 +50,22 @@ class SetPermissionTestCase(TestCase):
         )
 
     def test_set_permission(self):
+        """
+        Testa a função de permissões `set_permission`.
+        """
         content_type = ContentType.objects.get_for_model(Usuario)
 
+        # Define as permissões do usuário Tesoureiro Sede
         usuario_ts = set_permission(self.usuario_ts)
         self.assertIn(Group.objects.get(name='tesoureiro_sede'), usuario_ts.groups.all())
         self.assertTrue(usuario_ts.has_perm('accounts.tesoureiro_sede'))
 
+        # Define as permissões do usuário Tesoureiro
         usuario_t = set_permission(self.usuario_t)
         self.assertIn(Group.objects.get(name='tesoureiro'), usuario_t.groups.all())
         self.assertTrue(usuario_t.has_perm('accounts.tesoureiro'))
 
+        # Define as permissões do usuário Pastor
         usuario_p = set_permission(self.usuario_p)
         self.assertIn(Group.objects.get(name='pastor'), usuario_p.groups.all())
         self.assertTrue(usuario_p.has_perm('accounts.pastor'))
