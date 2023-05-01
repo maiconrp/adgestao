d010d56fbdaba602451e6db93f86698d1f76a04a
Maicon Robert <maiconlk2321@gmail.com>
2023-05-01 17:25:12 -0300

    :lock: feat: Configurar login

--- code/adgestao/settings.py
+++ code/adgestao/settings.py
@@ -135,3 +135,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 PROJECT_ROOT = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(PROJECT_ROOT, '../apps'))
+
+LOGIN_REDIRECT_URL = "/home"
--- code/adgestao/urls.py
+++ code/adgestao/urls.py
@@ -19,5 +19,6 @@ import accounts.urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include(accounts.urls))
+    path("accounts/", include(accounts.urls)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
