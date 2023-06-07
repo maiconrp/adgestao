410f2d1c20f7fce2d9b678d34d13254fbbd338c7
victorwilva <victorwilva@gmail.com>
2023-06-07 16:35:04 -0300

    :tada: feat: create form para dízimo

--- code/apps/igreja/forms.py
+++ code/apps/igreja/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from .models import Igreja, Membro
+from .models import Igreja, Membro, Dizimo, Oferta
 
 class IgrejaForm(forms.ModelForm):
 
@@ -9,8 +9,23 @@ class IgrejaForm(forms.ModelForm):
         model = Igreja
         fields = ['nome', 'localizacao', 'saldo']
 
+
 class MembroForm(forms.ModelForm):
 
     class Meta:
         model = Membro
         fields = ['nome', 'data_nasc', 'sexo', 'cpf']
+
+
+class DizimoForm(forms.ModelForm):
+
+    class Meta:
+        model = Dizimo
+        fields = ['data', 'valor', 'membro']
+
+
+class OfertaForm(forms.ModelForm):
+
+    class Meta:
+        model = Oferta
+        fields = ['data_culto', 'tipo_culto', 'valor_oferta']
