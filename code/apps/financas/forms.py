238bd59e4026508e95c67be7bd329c79d5f4242c
victorwilva <victorwilva@gmail.com>
2023-06-07 16:30:45 -0300

    :sparkles: feat: Add form para Dízimo

--- code/apps/financas/forms.py
+++ code/apps/financas/forms.py
@@ -2,10 +2,17 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.core.exceptions import ValidationError
 
-from .models import Saida
+from .models import Saida, Entrada
 
 class SaidaForm(forms.ModelForm):
 
     class Meta:
         model = Saida
-        fields = ['descricao', 'data', 'valor', 'igreja']
+        fields = ['descricao', 'data', 'valor']
+
+
+class EntradaForm(forms.ModelForm):
+
+    class Meta:
+        model = Entrada
+        fields = ['total', 'igreja']
