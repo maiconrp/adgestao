add86da7c4d28004c6fcdb7c35be3349b0f4b107
victorwilva <victorwilva@gmail.com>
2023-06-21 00:55:30 -0300

    :wrench: feat: Alterar registro no admin de 'Oferta' para 'OfertaCulto'

--- code/apps/igreja/admin.py
+++ code/apps/igreja/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Dizimo)
-admin.site.register(Oferta)
+admin.site.register(OfertaCulto)
 admin.site.register(Membro)
 admin.site.register(Igreja)
