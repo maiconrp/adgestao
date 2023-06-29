f0aae194facc35abf5a11fe30d807a88c312dc12
Voctor-367 <victorwilva@gmail.com>
2023-06-29 00:21:57 -0300

    :sparkles: code: Alterar redirecionamento de login

--- code/adgestao/settings.py
+++ code/adgestao/settings.py
@@ -136,7 +136,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 PROJECT_ROOT = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(PROJECT_ROOT, '../apps'))
 
-LOGIN_REDIRECT_URL = "/home"
+LOGIN_REDIRECT_URL = "/accounts/home"
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ str(BASE_DIR) + "/static"]
