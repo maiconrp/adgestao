96fdc73b09c848524939bb339f1f0885fa4f0cf3
Maicon Robert <60708311+maiconrp@users.noreply.github.com>
2023-07-19 10:22:38 -0300

    Update settings.py

--- code/adgestao/settings.py
+++ code/adgestao/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-bj_xd+7+^kae#l$2%s)zbz=h(s^=n_xosy!h$ri=g#f+248p4c"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -143,9 +143,3 @@ STATICFILES_DIRS = [str(BASE_DIR) + "/static"]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST_USER = 'maiconlk2321@gmail.com'
-EMAIL_HOST_PASSWORD = 'zapkjzxyjvwxmpjo'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-DEFAULT_FROM_EMAIL = 'maiconlk2321@gmail.com'
